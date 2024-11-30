e49c470ae4a2a03939f56fe54c4174d38de63308
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2024-11-30 18:32:23 +0530

    Updated Files

--- WEEK 1/script.js
+++ WEEK 1/script.js
@@ -3,9 +3,20 @@ function VenkyASyncFunction(callback){
     callback("Hi There")
 }
 
+let promise = new Promise(function(){
+    alert("Hello This is Promise")
+    resolve(56)
+})
+
+setTimeout(function(){
+    console.log("Helllo World")
+}, 2000)
+
 async function main() {
     VenkyASyncFunction(function(value){
         console.log(value)
     })
 }
+
+console.log(promise)
 main();
